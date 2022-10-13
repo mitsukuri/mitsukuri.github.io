d54afda6c6b3055048557490fbd2e4a8fa9256da
user <mail>
2022-10-13 15:06:03 +0300

    + 404

---
+++ pages/404.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function E404 () {
+  return <>
+  <h1>The undefined side of the force it seems like you&apos;ve landed on!</h1>
+  <Link href={'/'}>
+    Back you go, padavan, and wisely your side choose!
+  </Link>
+  </>
+}
