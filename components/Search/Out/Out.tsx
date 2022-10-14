d5c50663ba097d8de87853083ffc9c342a812be9
user <mail>
2022-10-14 10:05:28 +0300

    1st fetch

--- components/Search/Out/Out.tsx
+++ components/Search/Out/Out.tsx
@@ -1,6 +1,6 @@
 import style from "./Out.module.css";
 
-export default function Out ({data}) {
+export default function Out ({data} : {data : string}) {
   return <div className={style.root}>
     <div className={style.out}>
       {data}
--- components/Search/Search.tsx
+++ components/Search/Search.tsx
@@ -5,13 +5,13 @@ import Out from "./Out/Out";
 export default function Search () {
   const inpuContext = createContext ('');
   const [expanded, setExpanded] = useState (true);
-  const [data, setData] = useState ({});
+  const [data, setData] = useState ('');
 
   useEffect (() => {
     (async () => {
        const data = await fetch ('https://swapi.dev/api/people/?search=darth');
        const json = await data.text ();
-       setData (json);
+       setData (json.toString ());
     })();
   });
 
