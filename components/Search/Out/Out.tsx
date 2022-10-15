fd2443dbc5a26dc038d1e1a0d12413df4a6047b4
user <mail>
2022-10-15 17:19:41 +0300

    start implementing Entry

--- components/Search/Out/Out.tsx
+++ components/Search/Out/Out.tsx
@@ -2,15 +2,21 @@ import { Dispatch, useContext }    from "react";
 
 import style                       from "./Out.module.css";
 import { SearchAction, SearchCtx } from '../Search';
+import Entry from "./Entry/Entry";
+import { SwapiPeople } from "../../../interfaces/swapi-people";
 
 export default function Out ({dispatch} : {dispatch : Dispatch <SearchAction>}) {
   const ctx = useContext (SearchCtx);
 
   function renderContent () {
     return (
-    <li>
+    /*<li>
       {ctx.data.map ((d,i) => <ul key={i}>{d.name}</ul>)}
-    </li>)
+    </li>)}*/
+    <>
+      {ctx.data.map ((d,i) => <Entry key={i} data={d}/>)}
+    </>
+    );
   }
   return <div className={style.root}>
     <div className={style.out}>
