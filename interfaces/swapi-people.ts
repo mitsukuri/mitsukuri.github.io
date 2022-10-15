883e4e03a3d7cde231aca16be1bc099dac8c12da
user <mail>
2022-10-15 19:18:36 +0300

    omit commas in interface

--- interfaces/swapi-people.ts
+++ interfaces/swapi-people.ts
@@ -1,18 +1,18 @@
 export interface SwapiPeople {
-  name : string,
-  height : string,
-  mass : string,
-  hair_color : string,
-  skin_color : string,
-  eye_color : string,
-  birth_year : string,
-  gender : string,
-  homeworld : string,
-  film_urls : Array <string>,
-  species_urls : Array <string>,
-  vehicle_urls : Array <string>,
-  starship_urls : Array <string>,
-  url: string,
-  created: string,
+  name : string
+  height : string
+  mass : string
+  hair_color : string
+  skin_color : string
+  eye_color : string
+  birth_year : string
+  gender : string
+  homeworld : string
+  film_urls : Array <string>
+  species_urls : Array <string>
+  vehicle_urls : Array <string>
+  starship_urls : Array <string>
+  url: string
+  created: string
   edited: string
 }
