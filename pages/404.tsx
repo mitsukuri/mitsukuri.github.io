8c3cf1c1b2d1d232144a1888294cc8166d616990
user <mail>
2022-10-16 20:03:36 +0300

    ~404

--- pages/404.tsx
+++ pages/404.tsx
@@ -7,11 +7,11 @@ export default function E404 () {
   const r = useRouter ();
 
   return (
-  <div className={s.root}>
+  <div className={s.root} suppressHydrationWarning>
     <div className={s.box}>
       <div className={s.CDIV}>404</div>
-      <div className={s.message} suppressHydrationWarning>
-        Hrrrm!.. by <span className={s.url}>{r.asPath}</span> what you do mean?<br/>The undefined side of the force your path has led you to!
+      <div className={s.message}>
+        Hrrrm!.. by <span className={s.url}>{r.asPath}</span> what you do mean?<br/><span className={s.smaller}>The undefined side of the force your path has led you to!</span>
       </div>
       <button className={s.button} onClick={() => r.back ()}>
         Back you go and your side choose wisely!
